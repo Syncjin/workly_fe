fbb2f595f1278ad6402201078f588698829cd910
이우진 <dldndldms@gmail.com>
2025-07-18 12:04:42 +0900

    feat: SVG 파일 처리 방식 개선 및 타입 정의 추가. @svgr/webpack 의존성 재설정.

--- next.config.ts
+++ next.config.ts
@@ -3,11 +3,30 @@ import { createVanillaExtractPlugin } from "@vanilla-extract/next-plugin";
 
 const nextConfig: NextConfig = {
   webpack(config) {
-    config.module.rules.push({
-      test: /\.svg$/,
-      issuer: /\.[jt]sx?$/,
-      use: ["@svgr/webpack"],
-    });
+    const fileLoaderRule = config.module.rules.find((rule) => rule.test?.test?.(".svg"));
+
+    config.module.rules.push(
+      {
+        ...fileLoaderRule,
+        test: /\.svg$/i,
+        resourceQuery: /url/,
+      },
+      {
+        test: /\.svg$/i,
+        issuer: fileLoaderRule.issuer,
+        resourceQuery: { not: [...fileLoaderRule.resourceQuery.not, /url/] },
+        use: [
+          {
+            loader: "@svgr/webpack",
+            options: {
+              typescript: true,
+              ext: "tsx",
+            },
+          },
+        ],
+      }
+    );
+    fileLoaderRule.exclude = /\.svg$/i;
     return config;
   },
 };
