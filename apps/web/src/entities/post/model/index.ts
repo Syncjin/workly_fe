0a8a6ef6a13366a36dfae8b0592ac0b92121ba49
Syncjin <dldndldms@naver.com>
2025-10-13 00:49:54 +0900

    feat(web): post update api 및 쿼리 추가

--- apps/web/src/entities/post/model/index.ts
+++ apps/web/src/entities/post/model/index.ts
@@ -1,5 +1,5 @@
 /**
  * Post model exports
  */
-export { postQueryKeys } from "./keys";
-export type { Post, PostDeleteRequest, PostListParams, PostReadRequest } from "./types";
+export * from "./keys";
+export * from "./types";
