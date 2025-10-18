1c4953373a00d541ee5d6a915e132527a0ebff44
Syncjin <dldndldms@naver.com>
2025-10-18 16:41:35 +0900

    feat(entities): CommentItem ui 추가

---
+++ apps/web/src/entities/comment/ui/CommentItem.tsx
@@ -0,0 +1,117 @@
+import { Comment } from "@/entities/comment/model";
+import { formatYMDHM } from "@/shared/lib";
+import { Avatar, Button, cx } from "@workly/ui";
+import { createContext, useContext } from "react";
+import * as styles from "./commentItem.css";
+
+/** Context */
+type CtxValue = {
+  comment: Comment;
+  active?: boolean;
+  replyOnClick?: (comment: Comment) => void;
+};
+const Ctx = createContext<CtxValue | null>(null);
+const useItem = () => {
+  const v = useContext(Ctx);
+  if (!v) throw new Error("CommentItem.* must be used within <CommentItem.Root>");
+  return v;
+};
+
+function Profile() {
+  const { comment } = useItem();
+  return <Avatar showDot size="md" />;
+}
+
+function Author() {
+  const { comment } = useItem();
+  return <span className={styles.author}>이름</span>;
+}
+
+function Date() {
+  const { comment } = useItem();
+  let date = comment.updatedDateTime ? formatYMDHM(comment.updatedDateTime) : formatYMDHM(comment.createdDateTime);
+  return <span className={styles.date}>{date}</span>;
+}
+
+function Content() {
+  const { comment } = useItem();
+  return <p className={styles.content}>{comment.content}</p>;
+}
+
+function ReplyButton() {
+  const { replyOnClick, comment } = useItem();
+  return (
+    <Button variant="border" size="md" color="gray-300" onClick={() => replyOnClick?.(comment)}>
+      답글
+    </Button>
+  );
+}
+
+/** Slots: 레이아웃을 구성하는 영역들 */
+function HeaderSlot({ children }: { children?: React.ReactNode }) {
+  return (
+    <div className={styles.header}>
+      {children ?? (
+        <>
+          <Author />
+          <Date />
+        </>
+      )}
+    </div>
+  );
+}
+
+function ContentSlot({ children }: { children?: React.ReactNode }) {
+  return <div className={styles.contentArea}>{children ?? <Content />}</div>;
+}
+
+function FooterSlot({ children }: { children?: React.ReactNode }) {
+  return <div className={styles.footer}>{children ?? <ReplyButton />}</div>;
+}
+
+function RightSlot({ children }: { children?: React.ReactNode }) {
+  return <div className={styles.right}>{children}</div>;
+}
+
+/** Root */
+type RootProps = React.PropsWithChildren<
+  CtxValue & {
+    as?: "li" | "div";
+    className?: string;
+  }
+>;
+
+function Root({ as = "li", className, children, ...ctx }: RootProps) {
+  const Comp: any = as;
+  return (
+    <Ctx.Provider value={ctx}>
+      <Comp role={as === "li" ? "listitem" : undefined} className={cx(styles.container, className)}>
+        {/* 기본 레이아웃 제공 */}
+        {children ?? (
+          <>
+            <Avatar />
+            <div className={styles.main}>
+              <HeaderSlot />
+              <ContentSlot />
+              <FooterSlot />
+            </div>
+            <RightSlot />
+          </>
+        )}
+      </Comp>
+    </Ctx.Provider>
+  );
+}
+
+export const CommentItem = {
+  Root,
+  Profile,
+  Author,
+  Date,
+  Content,
+  ReplyButton,
+  HeaderSlot,
+  ContentSlot,
+  FooterSlot,
+  RightSlot,
+};
---
+++ apps/web/src/entities/comment/ui/commentItem.css.ts
@@ -0,0 +1,56 @@
+import { style } from "@vanilla-extract/css";
+
+export const right = style({
+  position: "absolute",
+  right: 0,
+  top: 0,
+});
+
+export const author = style({
+  fontSize: 14,
+  color: "var(--color-gray-900)",
+  fontWeight: 400,
+  lineHeight: "17px",
+  overflow: "hidden",
+  textOverflow: "ellipsis",
+  whiteSpace: "nowrap",
+  display: "inline-block",
+});
+
+export const date = style({
+  fontSize: 12,
+  color: "var(--color-gray-700)",
+  lineHeight: "17px",
+  display: "inline-block",
+});
+
+export const content = style({
+  fontSize: 14,
+  color: "var(--color-gray-900)",
+  lineHeight: "22px",
+});
+
+export const header = style({
+  display: "flex",
+  alignItems: "center",
+  position: "relative",
+});
+
+export const contentArea = style({
+  padding: "8px 16px 0 0",
+  wordWrap: "break-word",
+  wordBreak: "break-word",
+});
+
+export const footer = style({
+  marginTop: 12,
+});
+
+export const container = style({
+  position: "relative",
+  padding: "16px 0",
+});
+
+export const main = style({
+  margin: "0 0 0 56px",
+});
